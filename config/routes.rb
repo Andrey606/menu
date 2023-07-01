7dab840e0f5a16ab6d983254e25d682fe326a4a0
Andrii Kuluiev <akuluev@ukr.net>
2023-07-01 23:34:27 +0300

    admin_logout_path

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
-    delete 'logout', to: 'sessions#destroy'
+    get 'logout', to: 'sessions#destroy'
   end  
 
   get 'open_pdf/:id', to: 'admin/pdf_files#open_pdf', as: 'open_pdf'
